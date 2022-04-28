725a0dc091c4af3e9ab32e1d41cb25b30d7f27b6
Christian Donkjær Mønsted Nielsen <10724@student.eadania.dk>
2022-04-28 12:27:34 +0200

    nyt window

--- Projekt-HjemIS/Window1.xaml.cs
+++ Projekt-HjemIS/Window1.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Projekt_HjemIS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 window1 = new Window1();
-            window1.Show();
-            this.Close();
         }
     }
 }
