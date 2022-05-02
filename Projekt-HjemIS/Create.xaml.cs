e90b42db615fda472d5adce6ee69b4cdfd47bee2
Christian Donkjær Mønsted Nielsen <10724@student.eadania.dk>
2022-05-02 13:23:42 +0200

    Create kode lavet

--- Projekt-HjemIS/Create.xaml.cs
+++ Projekt-HjemIS/Create.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +28,67 @@ namespace Projekt_HjemIS
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection connString = new SqlConnection(ConfigurationManager.ConnectionStrings["path"].ConnectionString);
+            connString.Open();
+            try
+            {
+                if (Username.Text == "" || Username.Text == "Username" || Password.Password == "" || Password2.Password == "")
+                {
+                    MessageBox.Show("Please Fill Out The Required Fields");
+                    if (Username.Text == "" || Username.Text == "Username")
+                    {
+                        Username.BorderBrush = Brushes.Red;
+                    }
+                    if (Password.Password == "")
+                    {
+                        Password.BorderBrush = Brushes.Red;
+                    }
+                    if (Password2.Password == "")
+                    {
+                        Password2.BorderBrush = Brushes.Red;
+                    }
+                    
+                }
+                else if (Password.Password != Password2.Password)
+                {
+                    MessageBox.Show("Your Password Does Not Match");
+                }
+                else
+                {
+                    bool exists = false;
+                    using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) from Users WHERE [username] = @username", connString))
+                    {
+                        cmd.Parameters.AddWithValue("username", Username.Text);
+                        exists = (int)cmd.ExecuteScalar() > 0;
+                    }
+
+                    if (exists)
+                    {
+                        MessageBox.Show($"This Username {Username.Text} Already Exists");
+                    }
+                    else
+                    {
+                        string b = string.Empty;
+                        
+                        SqlCommand command = new SqlCommand($"INSERT INTO Users (username, [Password]) VALUES ('{Username.Text}', '{Password.Password}')", connString);
+                        command.ExecuteNonQuery();
+                        MessageBoxResult result = MessageBox.Show("User Has Been Created Succesfully", "User Record", MessageBoxButton.OK, MessageBoxImage.Information);
+                        if (result == MessageBoxResult.OK)
+                        {
+                            this.Close();
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                connString.Close();
+            }
 
         }
 
