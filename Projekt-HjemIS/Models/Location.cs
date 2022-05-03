1eb96ee0d03491ddddd22829a19c74764f222f42
Jonas Henckel Phørcksen Klausen <10687@student.eadania.dk>
2022-05-03 09:59:56 +0200

    Location model

---
+++ Projekt-HjemIS/Models/Location.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_HjemIS.Models
+{
+    public class Location
+    {
+        public string PostNr { get; set; }
+        public string Bynavn { get; set; }
+        public string Postdistrikt { get; set; }
+        public string Vejkode { get; set; }
+        public string Kommunekode { get; set; }
+        public string VejNavn { get; set; }
+        public string Distrikt { get; set; }
+        public Location(string postnr, string bynavn, string postdistrikt, string vejkode, string kommunekode)
+        {
+            PostNr = postnr;
+            Bynavn = bynavn;
+            Postdistrikt = postdistrikt;
+            Vejkode = vejkode;
+            Kommunekode = kommunekode;
+        }
+        public Location(string vejkode, string kommunekode)
+        {
+            Vejkode = vejkode;
+            Kommunekode = kommunekode;
+        }
+    }
+}
