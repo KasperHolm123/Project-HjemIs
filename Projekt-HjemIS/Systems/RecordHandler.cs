b199aea57adb824e365d6fc56e61e181e7cf57af
Kasper Holm <10669@student.eadania.dk>
2022-05-04 13:17:21 +0200

    GetRecords refactor

--- Projekt-HjemIS/Systems/RecordHandler.cs
+++ Projekt-HjemIS/Systems/RecordHandler.cs
@@ -18,85 +18,73 @@ namespace Projekt_HjemIS.Systems
         public RecordHandler()
         {
             Task.Factory.StartNew(() => Pulse());
-            ReadRecordFromFile();
+            GetRecords();
         }
 
-        List<string> RecordSegments = new List<string>() { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,
+        // Holds a single record.
+        private List<string> RecordSegments = new List<string>() { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,
             string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
 
-        // This dictionary holds all record types and their segment positional values. Refactor
+        // This dictionary holds all record types and their segment positional values.
         private Dictionary<string, int[]> RecordTypeDict = new Dictionary<string, int[]>()
         {
             { "001", new int[]{ 3, 4, 4, 12, 4, 4, 4, 4, 12, 20, 40 } }, // AKTVEJ
-            //{ "002", new int[]{ 3, 4, 4, 4, 2, 4, 12, 1, 12, 12, 34} }, // BOLIG
             { "003", new int[]{ 3, 4, 4, 4, 4, 1, 12, 34 } }, // BYNAVN
             { "004", new int[] { 3, 4, 4, 4, 4, 1, 12, 4, 20 }}, //POSTDIST
-            //{ "005", new int[]{ 3, 4, 4, 2, 40, 12, 12 } }, // NOTATVEJ
-            //{ "006", new int[]{ 3, 4, 4, 4, 4, 1, 12, 6, 30 } }, // BYFORNYDIST
-            //{ "007", new int[]{ 3, 4, 4, 4, 4, 1, 12, 2, 4, 30 } }, // DIVDIST
-            //{ "008", new int[]{ 3, 4, 4, 4, 4, 1, 12, 1, 30 } }, // EVAKUERDIST
             { "009", new int[]{ 3, 4, 4, 4, 4, 1, 12, 2, 30 } }, // KIRKEDIST
-            //{ "010", new int[]{ 3, 4, 4, 4, 4, 1, 12, 2, 30 } }, // SKOLEDIST
-            //{ "011", new int[]{ 3, 4, 4, 4, 4, 1, 12, 4, 30 } }, // BEFOLKDIST
-            //{ "012", new int[]{ 3, 4, 4, 4, 4, 1, 12, 2, 30 } }, // SOCIALDIST
-            //{ "013", new int[]{ 3, 4, 4, 4, 4, 1, 12, 4, 20 } }, // SOGNEDIST
-            //{ "014", new int[]{ 3, 4, 4, 4, 4, 1, 12, 2, 30 } }, // VALGDIST
-            //{ "015", new int[]{ 3, 4, 4, 4, 4, 1, 12, 4, 30 } }, // VARMEDIST
         };
 
         /// <summary>
         /// Read from .txt file
         /// </summary>
-        private void ReadRecordFromFile()
+        private void GetRecords()
         {
+            Stopwatch sw = new Stopwatch();
+            
             // Clear database tables to make room for a new data extraction.
             DatabaseHandler.ClearTables();
 
             // Keep count of how many record have been decoded and sent to the database.
             int recordCount = 0;
 
-            var rootPathChild = Directory.GetCurrentDirectory();
-            var rootPathParent = Directory.GetParent($"{rootPathChild}");
-            var rootPathFolder = Directory.GetParent($"{rootPathParent}");
-            var rootPath = rootPathFolder.ToString();
+            Location currentLocation = new Location();
 
-            Location model = new Location();
             string currentLine = string.Empty;
-            Stopwatch sw = new Stopwatch();
             List<Location> locationsList = new List<Location>();
-            using (StreamReader sr = File.OpenText(rootPath + @"\full.txt"))
+            using (StreamReader sr = File.OpenText(GetCurrentDirectory() + @"\full.txt"))
             {
                 sw.Start();
-                List<string> tempList = new List<string>();
+
+                // Read each line from current file.
                 while ((currentLine = sr.ReadLine()) != null)
                 {
-                    string tempLine = currentLine.Substring(0, 3);
-                    if (RecordTypeDict.Keys.Contains(currentLine.Substring(0, 3)))
-                        tempList = SpliceRecord(currentLine, RecordTypeDict[tempLine]);
+                    string currentLineRecordType = currentLine.Substring(0, 3);
 
                     // Instantiate a new Location object if the current line is a record of type "001".
-                    if (tempLine == "001")
+                    if (currentLineRecordType == "001")
                     {
                         // The first record doesn't have any data yet.
-                        if (recordCount > 0)
-                            locationsList.Add(model);
-                        model = new Location();
-                        //DatabaseHandler.AddData(model);   
+                        if (recordCount > 1)
+                            locationsList.Add(currentLocation);
+                        currentLocation = new Location(); 
                     }
-                    recordCount++;
 
-                    if (currentLine.Substring(0, 3) != "000")
-                        BuildLocation(model, tempList);
+                    if (RecordTypeDict.Keys.Contains(currentLine.Substring(0, 3)))
+                        BuildLocation(currentLocation, SpliceRecord(currentLine, RecordTypeDict[currentLineRecordType]));
 
                     // Keep count of handled records and total records
                     if (currentLine.Substring(0, 3) == "999")
                     {
-                        Debug.WriteLine("Amount of locations added to database: " + locationsList.Count);
-                        Debug.WriteLine("Total records: " + Int32.Parse(currentLine.Substring(4)) + "\n" + "Amount of handled records: " + (recordCount - 2));
-                        Debug.WriteLine((Int32.Parse(currentLine.Substring(4))) == recordCount - 2);
+                        Debug.WriteLine(
+                            "Amount of locations added to database: " + locationsList.Count + 
+                            "\nTotal records: " + Int32.Parse(currentLine.Substring(4)) + 
+                            "\nAmount of handled records: " + (recordCount - 2));
                     }
+
+                    recordCount++;
                 }
-                DatabaseHandler.AddData(locationsList);
+                //DatabaseHandler.AddData(locationsList);
+
                 Debug.WriteLine(sw.Elapsed);
                 sw.Stop();
             }
@@ -152,10 +140,22 @@ namespace Projekt_HjemIS.Systems
                 default:
                     loc.Distrikt = record[8];
                     break;
-
-
             }
         }
+
+        /// <summary>
+        /// Returns current directory.
+        /// </summary>
+        /// <returns></returns>
+        private string GetCurrentDirectory()
+        {
+            var rootPathChild = Directory.GetCurrentDirectory();
+            var rootPathParent = Directory.GetParent($"{rootPathChild}");
+            var rootPathFolder = Directory.GetParent($"{rootPathParent}");
+            var rootPath = rootPathFolder.ToString();
+            return rootPath;
+        }
+
         private async void Pulse()
         {
             while (true)
