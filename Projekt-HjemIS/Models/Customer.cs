eca5ac620e9ce195d2c28f71e765d52c72c4ec42
Kasper Holm <10669@student.eadania.dk>
2022-05-06 12:29:59 +0200

    Models

---
+++ Projekt-HjemIS/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_HjemIS.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public Location Address { get; set; }
+        public List<Message> MsgReceived { get; set; }
+
+
+        public Customer()
+        {
+
+        }
+    }
+}
---
+++ Projekt-HjemIS/Systems/MessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using Projekt_HjemIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_HjemIS.Systems
+{
+    public class MessageHandler
+    {
+        public List<Message> Messages { get; set; }
+        public List<Customer> Customers { get; set; }
+
+        public MessageHandler()
+        {
+
+        }
+
+        private void GetCustomers()
+        {
+
+        }
+
+        private void SendMessages()
+        {
+
+        }
+    }
+}
