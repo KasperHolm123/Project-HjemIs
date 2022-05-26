79ad29155af4cd639595c018eccff363d1a52efd
Daniel Bonde Rasmussen <15269@student.eadania.dk>
2022-05-26 10:27:46 +0200

    Offer view repurposed to EditCustomerView mock-up xaml laid out, no binds yet

--- Projekt-HjemIS/Views/EditCustomerView.xaml.cs
+++ Projekt-HjemIS/Views/EditCustomerView.xaml.cs
@@ -24,5 +24,10 @@ namespace Projekt_HjemIS.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
