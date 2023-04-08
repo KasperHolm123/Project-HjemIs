0e09705af994b1f752f6a66bf9207745ad700123
Kasper Holm <kasper.marhauer@gmail.com>
2023-04-08 11:30:26 +0200

    DatatProcessorService can now parse, upsert, and combine all forms of records

--- src/Projekt-HjemIS/Models/RecordTypes.cs
+++ src/Projekt-HjemIS/Models/RecordTypes.cs
@@ -7,89 +7,82 @@ using System.Threading.Tasks;
 namespace Projekt_HjemIS.Models
 {
 
-    public class RecordTypeOther
+    public class Record
     {
         public string RecordType { get; set; }
+    }
+
+    public class RecordTypeOther : Record
+    {
         public string CountyCode { get; set; }
         public string StreetCode { get; set; }
         public string HouseNumberFrom { get; set; }
         public string HouseNumberTo { get; set; }
         public string EvenOdd { get; set; }
-        public string TimeStamp { get; set; }
+        public string Timestamp { get; set; }
+
+        public RecordTypeOther(string[] record)
+        {
+            RecordType = record[0];
+            CountyCode = record[1];
+            StreetCode = record[2];
+            HouseNumberFrom = record[3];
+            HouseNumberTo = record[4];
+            EvenOdd = record[5];
+            Timestamp = record[6];
+        }
     }
 
-    public class AKTVEJ
+    public class RecordTypeAKTVEJ : Record
     {
+        public string CountyCode { get; set; }
+        public string StreetCode { get; set; }
+        public string Timestamp { get; set; }
+        public string ToCountyCode { get; set; }
+        public string ToStreetCode { get; set; }
+        public string FromCountyCode { get; set; }
+        public string FromStreetCode { get; set; }
+        public string ThereStart { get; set; }
+        public string StreetAddrName { get; set; }
+        public string StreetName { get; set; }
+
+        public RecordTypeAKTVEJ(string[] record)
+        {
+            RecordType = record[0];
+            CountyCode = record[1];
+            StreetCode = record[2];
+            Timestamp = record[3];
+            ToCountyCode = record[4];
+            ToStreetCode = record[5];
+            FromCountyCode = record[6];
+            FromStreetCode = record[7];
+            ThereStart = record[8];
+            StreetAddrName = record[9];
+            StreetName = record[10];
+        }
     }
 
-    public class BOLIG
+    public class RecordTypeBOLIG : Record
     {
-
+        public string CountyCode { get; set; }
+        public string StreetCode { get; set; }
+        public string HouseNumber { get; set; }
+        public string Floor { get; set; }
+        public string NextDoorNumber { get; set; }
+        public string Timestamp { get; set; }
+        public string Filler1a { get; set; }
+        public string ThereStart { get; set; }
+        public string Filler12N { get; set; }
+        public string Locality { get; set; }
     }
 
-    public class BYNAVN : RecordTypeOther
+    public class RecordTypeNOTATVEJ : Record
     {
-
+        public string CountyCode { get; set; }
+        public string StreetCode { get; set; }
+        public string NoteNumber { get; set; }
+        public string NoteLine { get; set; }
+        public string Timestamp { get; set; }
+        public string ThereStart { get; set; }
     }
-
-    public class POSTDIST : RecordTypeOther
-    {
-
-    }
-
-    public class NOTATVEJ
-    {
-
-    }
-
-    public class BYFORNYDIST : RecordTypeOther
-    {
-
-    }
-
-    public class DIVDIST : RecordTypeOther
-    {
-
-    }
-
-    public class EVAKUERDIST : RecordTypeOther
-    {
-
-    }
-
-    public class KIRKEDIST : RecordTypeOther
-    {
-
-    }
-
-    public class SKOLEDIST : RecordTypeOther
-    {
-
-    }
-
-    public class BEFOLKDIST : RecordTypeOther
-    {
-
-    }
-
-    public class SOCIALDIST : RecordTypeOther
-    {
-
-    }
-
-    public class SOGNEDIST : RecordTypeOther
-    {
-
-    }
-
-    public class VALGDIST : RecordTypeOther
-    {
-
-    }
-
-    public class VARMEDIST : RecordTypeOther
-    {
-
-    }
-
 }
--- src/Projekt-HjemIS/Services/DataProcessorService.cs
+++ src/Projekt-HjemIS/Services/DataProcessorService.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Projekt_HjemIS.Models;
+using Projekt_HjemIS.Systems.Utility.Database_handling;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,8 @@ namespace Projekt_HjemIS.Services
 {
     public class DataProcessorService
     {
+        private static readonly DatabaseHandler databaseHandler = new DatabaseHandler();
+
         private static Dictionary<string, int[]> _recordTypes = new Dictionary<string, int[]>
         {
             // All records are made up of sections with a pre-assigned
@@ -34,7 +40,7 @@ namespace Projekt_HjemIS.Services
             { "999", new int[] { } }
         };
 
-        public void ProcessData()
+        public async Task ProcessData()
         {
             var path = "../../dropzone/sample_data.txt";
 
@@ -43,27 +49,167 @@ namespace Projekt_HjemIS.Services
 
             using (var reader = new StreamReader(path))
             {
+                var records = new List<Record>();
+
                 var currentLine = "";
-
-
                 while ((currentLine = reader.ReadLine()) != null)
                 {
+                    // all data (of correct format) starts with a record-type as it's 3 first digits.
                     var recordType = currentLine.Substring(0, 3);
 
-                    var index = 0;
-                    
-                    foreach (var sectionLength in _recordTypes[recordType])
+                    var record = ParseRecord(currentLine, recordType);
+
+                    switch (record[0])
                     {
-                        Debug.Write(currentLine.Substring(index, sectionLength) + " ");
-                        index += sectionLength;
+                        case "001":
+                            records.Add(new RecordTypeAKTVEJ(record));
+                            break;
+                        case "002":
+                            records.Add(new RecordTypeBOLIG());
+                            break;
+                        case "005":
+                            records.Add(new RecordTypeNOTATVEJ());
+                            break;
+                        case "000":
+                        case "999":
+                            break;
+                        default:
+                            records.Add(new RecordTypeOther(record));
+                            break;
                     }
 
                     Debug.WriteLine("");
                 }
+
+                // upsert all parsed Record object to database
+                await SaveToDatabase(records);
+
+                await BuildLocations();
             }
 
             timer.Stop();
             Debug.WriteLine(timer.Elapsed);
+        }
+
+        /// <summary>
+        /// Builds Location elements from already existing Record elements (in the form of RecordType- Aktvej/Other)
+        /// and upserts them to database
+        /// </summary>
+        /// <returns></returns>
+        private static async Task BuildLocations()
+        {
+            var query = "INSERT INTO [Location] (StreetName, CountyCode, StreetCode, HouseNumberFrom, HouseNumberTo, EvenOdd) " +
+                        "SELECT AKTVEJ.StreetName, Other.CountyCode, Other.StreetCode, Other.HouseNumberFrom, Other.HouseNumberTo, Other.EvenOdd " +
+                        "FROM RecordTypeAKTVEJ AS AKTVEJ " +
+                        "JOIN RecordTypeOther AS Other " +
+                        "ON AKTVEJ.CountyCode = Other.CountyCode " +
+                        "AND AKTVEJ.StreetCode = Other.StreetCode " +
+                        "WHERE AKTVEJ.CountyCode = Other.CountyCode " +
+                        "AND AKTVEJ.StreetCode = Other.StreetCode;";
+
+            await databaseHandler.AddData(query);
+        }
+
+        private static async Task SaveToDatabase(List<Record> records)
+        {
+            foreach (var record in records)
+            {
+                // upload logic moved into separate methods for better readability
+                if (record.GetType() == typeof(RecordTypeAKTVEJ))
+                {
+                    await UploadAktvej(record);
+                }
+                else if (record.GetType() == typeof(RecordTypeOther))
+                {
+                    await UploadOther(record);
+                }
+            }
+        }
+
+        private static async Task UploadAktvej(Record record)
+        {
+            var convertedRecord = (RecordTypeAKTVEJ)record;
+
+            var query = "INSERT INTO RecordTypeAKTVEJ " +
+                        "(RecordType, CountyCode, StreetCode, [Timestamp], " +
+                        "ToCountyCode, ToStreetCode, FromCountyCode, FromStreetCode, " +
+                        "ThereStart, StreetAddrName, StreetName) " +
+                            "SELECT @RecordType, @CountyCode, @StreetCode, @Timestamp, " +
+                            "@ToCountyCode, @ToStreetCode, @FromCountyCode, @FromStreetCode," +
+                            " @ThereStart, @StreetAddrName, @StreetName " +
+                            "WHERE NOT EXISTS ( " +
+                            "SELECT 1 FROM RecordTypeAKTVEJ " +
+                            "WHERE StreetName = @StreetName " +
+                        ")";
+
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@RecordType", convertedRecord.RecordType),
+                new SqlParameter("@CountyCode", convertedRecord.CountyCode),
+                new SqlParameter("@StreetCode", convertedRecord.StreetCode),
+                new SqlParameter("@Timestamp", convertedRecord.Timestamp),
+                new SqlParameter("@ToCountyCode", convertedRecord.ToCountyCode),
+                new SqlParameter("@ToStreetCode", convertedRecord.ToStreetCode),
+                new SqlParameter("@FromCountyCode", convertedRecord.FromCountyCode),
+                new SqlParameter("@FromStreetCode", convertedRecord.FromStreetCode),
+                new SqlParameter("@ThereStart", convertedRecord.ThereStart),
+                new SqlParameter("@StreetAddrName", convertedRecord.StreetAddrName),
+                new SqlParameter("@StreetName", convertedRecord.StreetName),
+            };
+
+            await databaseHandler.AddData(query, parameters.ToArray());
+        }
+
+        private static async Task UploadOther(Record record)
+        {
+            var convertedRecord = (RecordTypeOther)record;
+
+            var query = "INSERT INTO RecordTypeOther " +
+                        "(RecordType, CountyCode, StreetCode, HouseNumberFrom, " +
+                        "HouseNumberTo, EvenOdd, [Timestamp]) " +
+                        "SELECT @RecordType, @CountyCode, @StreetCode, @HouseNumberFrom, @HouseNumberTo, @EvenOdd, @Timestamp " +
+                        "WHERE NOT EXISTS ( " +
+                            "SELECT 1 FROM RecordTypeOther " +
+                            "WHERE CountyCode = @CountyCode " +
+                            "AND StreetCode = @StreetCode " +
+                            "AND HouseNumberFrom = @HouseNumberFrom " +
+                            "AND HouseNumberTo = @HouseNumberTo " +
+                            "AND EvenOdd = @EvenOdd " +
+                        ")";
+
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@RecordType", convertedRecord.RecordType),
+                new SqlParameter("@CountyCode", convertedRecord.CountyCode),
+                new SqlParameter("@StreetCode", convertedRecord.StreetCode),
+                new SqlParameter("@HouseNumberFrom", convertedRecord.HouseNumberFrom),
+                new SqlParameter("@HouseNumberTo", convertedRecord.HouseNumberTo),
+                new SqlParameter("@EvenOdd", convertedRecord.EvenOdd),
+                new SqlParameter("@Timestamp", convertedRecord.Timestamp),
+            };
+
+            await databaseHandler.AddData(query, parameters.ToArray());
+        }
+
+        private static string[] ParseRecord(string loadedRecord, string type)
+        {
+            // used to keep track of which section we're at in the .
+            var readDigits = 0;
+
+            var record = new string[_recordTypes[type].Length];
+
+            for (int i = 0; i < record.Length; i++)
+            {
+                // building the record section by section
+                foreach (var sectionLength in _recordTypes[type])
+                {
+                    record[i] = loadedRecord.Substring(readDigits, sectionLength);
+                    readDigits += sectionLength;
+                    i++;
+                }
+            }
+
+            return record;
         }
     }
 }
